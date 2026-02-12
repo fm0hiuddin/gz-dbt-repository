d81bb38d0d4d91716629ec90fc996a6d8ff2e758
fariya.mohiuddin@gmail.com <fariya.mohiuddin@gmail.com>
2026-02-12 15:59:57 +0000

    created macro and used it

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -7,7 +7,8 @@
             s.quantity,
             p.purchase_price, --calculation follows
             ROUND(s.revenue-(s.quantity*p.purchase_price),2) as margin,
-            ROUND(s.quantity*p.purchase_price,2) as purchase_cost
+            ROUND(s.quantity*p.purchase_price,2) as purchase_cost,
+            {{ margin_percent( 's.revenue' ,  'p.purchase_price' ) }} AS margin_percent
     from {{ref("stg_raw__sales")}} s
     left join {{ref("stg_raw__product")}} p
     using (products_id)
